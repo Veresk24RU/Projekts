36090c652b566fdd8d13becceac0446c1438072b
Veresk24RU <gloriagipson1933@microevolutmail.com>
2025-10-02 15:38:44 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-console.log(1);
-
-const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-
-button.addEventListener('click', function() {output.innerHTML = 'Это блин текст для вывода по клику';});
